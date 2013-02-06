d7669f494909e63ab681481e6c3c8872f9c56d21
Bobby DeVeaux <me@bobbyjason.co.uk>
2013-02-06 13:16:52 +0000

    Broadcast to all connections

--- amqp.socket.js
+++ amqp.socket.js
@@ -25,9 +25,16 @@ rabbitMQ.on('ready', function() {
     rabbitMQ.queue('datafeed', { }, function(q) {
       q.bind('voucherExchange', '#'); // Catch all messages    
       q.subscribe(function (message) {
-        console.log('message rcvd' + message.data.toString());
+        console.log('message rcvd: ' + message.data.toString());
         obj = message.data.toString();
-        socket.emit('news', {data: obj});
+
+        // 2 ways to alternate between all connections
+        // socket.emit('news', {data: obj});
+        // socket.broadcast.to(obj.id).emit('news', {data: obj});
+
+        // 2 ways to truly broadcast to all connections
+        // io.sockets.in(obj.id).emit('news', {data: obj});
+        io.sockets.emit('news', {data: obj});
       });
     });
   });
